def2d0a431de4114a97ba160dfe178985e19ddd0
Tristan Caron <tristan.caron@unity3d.com>
2021-12-01 20:53:28 -0800

    Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,27 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: Sequence<String>) =
+        input
+            .map(String::toInt)
+            .drop(1)
+            .fold(0 to 0) { (sum, prev), current -> (if (current > prev) sum + 1 else sum) to current }
+            .first
+
+    fun part2(input: Sequence<String>) =
+        input
+            .map(String::toInt)
+            .windowed(3)
+            .map { it.sum() }
+            .drop(1)
+            .fold(0 to 0) { (sum, prev), current -> (if (current > prev) sum + 1 else sum) to current }
+            .first
+
+    readInput("Day01_test").also {
+        it.run(::part1).also { check(it == 7) }
+        it.run(::part2).also { check(it == 5) }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    readInput("Day01").also {
+        it.run(::part1).also(::println)
+        it.run(::part2).also(::println)
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
 }
